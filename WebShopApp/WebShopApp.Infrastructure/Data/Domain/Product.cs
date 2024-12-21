7e51db4cb87719f41137b466d21697fa31b33b66
AlexanderVasilev629 <a.vasilev2020@pmg-pernik.com>
2024-12-21 19:09:18 +0200

    Upload Project

--- WebShopApp/WebShopApp.Infrastructure/Data/Domain/Product.cs
+++ WebShopApp/WebShopApp.Infrastructure/Data/Domain/Product.cs
@@ -18,7 +18,9 @@ namespace WebShopApp.Infrastructure.Data.Domain
         [Required]
         public int BrandId { get; set; }
         public virtual Brand Brand { get; set; } = null!;
-
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; } = null!;
         public string Picture { get; set; } = null!;
 
         [Range(0, 5000)]
