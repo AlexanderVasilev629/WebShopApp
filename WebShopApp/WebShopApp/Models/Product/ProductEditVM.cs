8be5ae9513bb8d1b88ee46bd6f565c70de6fb794
AV12B <a.vasilev2020@pmg-pernik.com>
2025-01-16 11:36:29 +0200

    Index

---
+++ WebShopApp/WebShopApp/Models/Product/ProductEditVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebShopApp.Models.Product
+{
+    public class ProductEditVM
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [Display(Name = "ProductName")]
+        public string ProductName { get; set; } = null!;
+
+        
+
+    }
+}
