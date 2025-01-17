718cc674a09507825cfcc38270ac6966d2df2b2c
AV12B <a.vasilev2020@pmg-pernik.com>
2025-01-17 11:37:23 +0200

    Finish

--- WebShopApp/WebShopApp/Models/Product/ProductDeleteVM.cs
+++ WebShopApp/WebShopApp/Models/Product/ProductDeleteVM.cs
@@ -6,7 +6,7 @@ namespace WebShopApp.Models.Product
     {
         [Key]
         public int Id { get; set; }
-
+          
         [Display(Name = "Product Name")]
         public string ProductName { get; set; } = null!;
 
